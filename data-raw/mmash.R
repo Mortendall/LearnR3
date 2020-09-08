4e3d4e4b95093b4aa74b62a8e48f51668cd8896e
Morten Dall <dall@sund.ku.dk>
2020-09-08 11:44:05 +0200

    unzipped data files

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -9,4 +9,15 @@ getwd()
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
 download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 
-usethis::
+unzip(here("data-raw/mmash-data.zip"),
+      exdir = here("data-raw"),
+      junkpaths = T)
+
+unzip(here("data-raw/MMASH.zip"),
+      exdir = here("data-raw"))
+
+library(fs)
+file_delete(here(c("data-raw/MMASH.zip",
+                   "data-raw/SHA256SUMS.txt",
+                  "data-raw/LICENSE.txt")))
+file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
