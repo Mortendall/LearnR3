5ed9ad41792f241cfc7858b5cbb16df4b2b570b3
Morten Dall <dall@sund.ku.dk>
2020-09-08 17:16:24 +0200

    Added import file function, appended with purrr functions

--- R/functions.R
+++ R/functions.R
@@ -49,3 +49,65 @@ import_saliva_data <- function(import_path) {
   )
   return(saliva_data)
 }
+
+#' RR file importer
+#'
+#' @param file_path file directory
+#'
+#' @return the imported data
+
+import_rr_data <- function(file_path){
+  import_data <- vroom::vroom(
+    file_path,
+    col_select = -1,
+    col_types = vroom::cols(
+      ibi_s = vroom::col_double(),
+      day = vroom::col_double(),
+      time = vroom::col_time(format = "")),
+    .name_repair = snakecase::to_snake_case)
+  return(import_data)
+}
+
+#' Actigraph Data Importer
+#'
+#' @param file_path the file directory
+#'
+#' @return the imported data
+
+import_actigraph_data <- function(file_path) {
+  data_import <- vroom::vroom(
+    file_path,
+    col_select = -1,
+    col_types = vroom::cols(
+      Axis1 = vroom::col_double(),
+      Axis2 = vroom::col_double(),
+      Axis3 = vroom::col_double(),
+      Steps = vroom::col_double(),
+      HR = vroom::col_double(),
+      `Inclinometer Off` = vroom::col_double(),
+      `Inclinometer Standing` = vroom::col_double(),
+      `Inclinometer Sitting` = vroom::col_double(),
+      `Inclinometer Lying` = vroom::col_double(),
+      `Vector Magnitude` = vroom::col_double(),
+      day = vroom::col_double(),
+      time = vroom::col_time(format = "")
+    ),
+    .name_repair = snakecase::to_snake_case
+  )
+  return(data_import)
+}
+
+#' Import data sets
+#'
+#' @param file_pattern The file pattern we want to search for
+#' @param import_function The import function for this data type
+#'
+#' @return an assembled file with data from each user
+
+import_multiple_files <- function(file_pattern, import_function){
+  data_files <- fs::dir_ls(here::here("data-raw/mmash/"),
+                           regexp = file_pattern,
+                           recurse = T)
+  combined_data <- purrr::map_dfr(data_files,import_function, .id = "file_path_id" )
+  return(combined_data)
+}
